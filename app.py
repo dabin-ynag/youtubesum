f6265e12d76482ede8174383fb5b335430d81729
dabin-ynag <ekqlsl1128@gmail.com>
2024-11-06 23:23:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,50 @@ from dotenv import load_dotenv
 import streamlit as st
 import os
 import google.generativeai as genai
+from youtube_transcript_api import YouTubeTranscriptApi
  
 load_dotenv()   ## load all environment variables
  
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
  
-st.set_page_config(page_title="My Audio Summarizer",page_icon="🤖")
+st.set_page_config(page_title="Youtube Summarizer",page_icon="📺")
  
-st.header("My Audio Summarizer Web Application")
+st.header("My Youtube Summarizer Web Application")
  
-uploaded_audio = st.file_uploader("Upload an audio file to summarize",type=['mp3'])
+youtube_link = st.text_input("Enter Youtube Video Link")
  
-if uploaded_audio is not None:
-    ## Save the audio file to a temporary file
-    audio_file_name = uploaded_audio.name
-    with open(audio_file_name,"wb") as f:
-        f.write(uploaded_audio.getbuffer())
+if youtube_link:
+    try:
+        video_id = youtube_link.split("=")[1]
+        st.image("https://img.youtube.com/vi/{}/0.jpg".format(video_id),use_column_width=True)
+    except Exception as e:
+        st.write("Invalid Url")
  
-    st.audio(audio_file_name)
+submit = st.button("Summarize")
  
-    st.write("Uploading file...")
+prompt = """You are a YouTuve video summarizer, you will be taking the transcript text and summarize the
+entire video and provide the import summary. Please provide the summary of the text given here in two
+languages, English and Hindi within 250-300 words. Please translate accordingly. \n\n"""
  
-    audio_file = genai.upload_file(path=audio_file_name)
+def extract_transcript_details(video_url):
+    try:
+       video_id = youtube_link.split("=")[1]
+       transcript_text =  YouTubeTranscriptApi.get_transcript(video_id,languages=["en","ko"])
+       transcript = ""
+       for i in transcript_text:
+          transcript += " "+i['text']
+       return transcript
+    except Exception as e:
+        raise e
  
-    st.write("Upload Completed : {}".format(audio_file.uri))
- 
-    st.write("Generating the summary....")
- 
-    prompt = """Listen carefully to the following audio file and provide the brief summary 
-                in 200-250 words"""
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content([prompt,audio_file])
-    st.write(response.text)
+if submit:
+    try:
+        transcript_text = extract_transcript_details(youtube_link)
+        if transcript_text:
+            model = genai.GenerativeModel("gemini-pro")
+            summary = model.generate_content(prompt+transcript_text)
+            st.write(summary.text)
+        else:
+            st.write("Unable to summarize")
+    except Exception as e:
+        st.write("Unable to summarize")
